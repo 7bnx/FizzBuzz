c137446bd42480fa814a7fbaac40e6dbf3a0b460
7bnx <agreement90@mail.ru>
2023-11-13 22:00:12 +0300

    test: storage

---
+++ Tests/Test_MatchedStorage.cs
@@ -0,0 +1,35 @@
+﻿namespace FizzBuzz.Tests;
+
+[TestClass]
+public class Test_MatchedStorage
+{
+  [TestMethod]
+  public void AppendAndGetSameQuantityOfTags()
+  {
+    var testObject = new MatchedStorage();
+    int expectedQty = 12;
+    for (int i = 0; i < expectedQty; i++)
+      testObject.Append(new Mock<ITag>().Object);
+
+    var actualQty = testObject.GetTags().Count();
+
+    Assert.AreEqual(expectedQty, actualQty);
+  }
+
+  [TestMethod]
+  public void CheckSameOrderOfMatchedTags()
+  {
+    var testObject = new MatchedStorage();
+    var tagsStrings = new string[] { "fizz", "buzz", "guzz", "muzz" };
+    foreach (var s in tagsStrings)
+    {
+      var mockTag = new Mock<ITag>();
+      mockTag.SetupGet(t => t.Get).Returns(s);
+      testObject.Append(mockTag.Object);
+    }
+
+    var actualTags = testObject.GetTags();
+
+    Assert.IsTrue(tagsStrings.SequenceEqual(actualTags.Select(t => t.Get)));
+  }
+}
