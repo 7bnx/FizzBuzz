0eaf4877b559951dcb2b065656a374ffcfef219d
7bnx <agreement90@mail.ru>
2023-11-13 21:17:16 +0300

    feat: add extension method for add specification

---
+++ Common/ExtensionMethods.cs
@@ -0,0 +1,8 @@
+﻿namespace FizzBuzz.Common;
+
+public static class ExtensionMethods
+{
+
+  public static ISpecification And(this ISpecification spec1, ISpecification spec2)
+    => new AndSpecification(spec1, spec2);
+}
