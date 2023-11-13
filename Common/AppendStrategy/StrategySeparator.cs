7d70086d082dc094d60bbaeeb9d2edfe71c3d255
7bnx <agreement90@mail.ru>
2023-11-13 21:33:32 +0300

    feat: add append strategy

---
+++ Common/AppendStrategy/StrategySeparator.cs
@@ -0,0 +1,10 @@
+﻿namespace FizzBuzz.Common;
+
+public class StrategySeparator : IAppendStrategy
+{
+  private readonly string _separator;
+  public StrategySeparator(string separator)
+     => _separator = separator;
+  public string Execute(IEnumerable<ITag> tags)
+    => string.Join(_separator, tags.Select(t => t.Get));
+}
